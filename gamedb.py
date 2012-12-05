a87c74bff418d7300a78e28123cc5719520a51f4
Jonathan Gray <jonathan.sam.gray@gmail.com>
2012-12-05 13:19:10 -0500

    Added count_derived() to the database API

--- gamedb.py
+++ gamedb.py
@@ -35,19 +35,20 @@ def create_game(cid1, cid2):
 
 
 def make_move(cid, player, move):
-	colc = 'cid' + str(player)
-	colm = 'p' + str(move)
+	colc = 'cid' + str(player)	# This might be prone to SQL injections
+	colm = 'p' + str(move)	# This might be prone to SQL injections
 	dml = 'UPDATE games SET {}=? WHERE done=0 AND {}=?'.format(colm, colc)
 	c.execute(dml, (player, cid))
 	conn.commit()
 
 
 def game_over(cid, player, draw):
-	colc = 'cid' + str(player)
+	colc = 'cid' + str(player)	# This might be prone to SQL injections
 	winner = 0 if draw else player
 	dml = 'UPDATE games SET done=1, winner=? WHERE done=0 and {}=?'.format(colc)
 	c.execute(dml, (winner, cid))
 	conn.commit()
+
 
 def get_state(cid, done=False):
 	q = '''SELECT p0, p1, p2, p3, p4, p5, p6, p7, p8 
@@ -55,3 +56,22 @@ def get_state(cid, done=False):
 		   WHERE done=? AND (cid1=? OR cid2=?)  '''
 	c.execute(q, (done, cid, cid))
 	return c.fetchone()
+
+
+def count_derived(state):
+	# Searches for completed games that can be derived from a particular state
+	# Returns a tuple (a, b, c) where:
+	# 	a is the number of such games that resulted in a draw
+	# 	b is the number of such games won by player 1
+	# 	c is the number of such games won by player 2
+	where = ' AND '.join(['p{}={}'.format(i, state[i]) for i in xrange(9) if state[i] != 0])	# This might be prone to SQL injections
+	q = ''' SELECT winner, count(*)
+			FROM games
+			WHERE done=1 AND {}
+			GROUP BY winner '''.format(where)
+	c.execute(q)
+	result = c.fetchall()
+	r = [0, 0, 0]
+	for a, b in result:
+		r[a] = b
+	return tuple(r)
