5fe70371f2cc76cbaf77ccc94f1c03a05cae22fe
Out of Ink Software <moshekrum@gmail.com>
2017-07-23 12:27:19 -0500

    rewrote pentago (single computer version)

--- pentagoS.py
+++ pentagoS.py
@@ -1,225 +1,163 @@
-from os import system, name
+import os # only to clear the screen
 
 class Pentago:
 	def __init__(self):
-		self.play()
+		self.player = 1
+		self.playing = True
+		self.placing = True
+		self.gameBoard = [[" " for i in range(9)] for j in range(4)]
 
-	def genBoard(self):
-		newboard = [[" " for i in range(9)] for j in range(4)]  
-		return newboard
+		self.ps = {1:"W", 2:"B"}
+		self.players = {1: "White", 2: "Black"}
 
-	def prettyBoard(self, board):
-		mboard = """\
-	{0[0][0]} {0[0][1]} {0[0][2]}|{0[1][0]} {0[1][1]} {0[1][2]}
-	{0[0][3]} {0[0][4]} {0[0][5]}|{0[1][3]} {0[1][4]} {0[1][5]}
-	{0[0][6]} {0[0][7]} {0[0][8]}|{0[1][6]} {0[1][7]} {0[1][8]}
-	-----+-----
-	{0[2][0]} {0[2][1]} {0[2][2]}|{0[3][0]} {0[3][1]} {0[3][2]}
-	{0[2][3]} {0[2][4]} {0[2][5]}|{0[3][3]} {0[3][4]} {0[3][5]}
-	{0[2][6]} {0[2][7]} {0[2][8]}|{0[3][6]} {0[3][7]} {0[3][8]}""".format(board)
-		return mboard
+	def switchPlayers(self):
+		self.player = 3 - self.player
 
-	def rotateCC(self, board, sector):
-		nboard=self.genBoard()
+	def get2dArray(self):
+		#make 2d array of each corner
+		corner = []
+
 		for x in range(4):
-			if x != sector:
-				nboard[x]=board[x]
-		nboard[sector][0] = board[sector][2]
-		nboard[sector][1] = board[sector][5]
-		nboard[sector][2] = board[sector][8]
-		nboard[sector][3] = board[sector][1]
-		nboard[sector][4] = board[sector][4]
-		nboard[sector][5] = board[sector][7]
-		nboard[sector][6] = board[sector][0]
-		nboard[sector][7] = board[sector][3]
-		nboard[sector][8] = board[sector][6]
-		return nboard
+			corner.append([self.gameBoard[x][i:i + 3] for i in range(0, len(self.gameBoard[x]), 3)])
+		
+		#make and fill 2d array of the whole board
+		board = []
 
-	def rotateC(self, board, sector):
-		nboard=self.genBoard()
-		for x in range(4):
-			if x != sector:
-				nboard[x]=board[x]
-		nboard[sector][2] = board[sector][0]
-		nboard[sector][5] = board[sector][1]
-		nboard[sector][8] = board[sector][2]
-		nboard[sector][1] = board[sector][3]
-		nboard[sector][4] = board[sector][4]
-		nboard[sector][7] = board[sector][5]
-		nboard[sector][0] = board[sector][6]
-		nboard[sector][3] = board[sector][7]
-		nboard[sector][6] = board[sector][8]
-		return nboard
-
-	def printBoard(self, board, spacing=0):
-		system('cls' if name == 'nt' else 'clear')
-		print self.prettyBoard(board)
-		print
-
-	def rotateSquare(self, board):
-		while True:
-			pass0 = False
-			pass1 = False
-			pass2 = False
-			rawrot=raw_input("Rotation (e.g. A' or C\"): ")
-			if len(rawrot) == 2:
-				pass0 = True
-			if pass0:
-				for mark in ["'",'"']:
-					if rawrot.endswith(str(mark)):
-						pass1 = True
-				for letter in ["A", "B", "C", "D"]:
-					if rawrot.startswith(letter):
-						pass2 = True
-			if pass1 and pass2:
-				if rawrot.startswith("A"):
-					square = 0
-				elif rawrot.startswith("B"):
-					square = 1
-				elif rawrot.startswith("C"):
-					square = 2
-				else:
-					square = 3
-				if rawrot.endswith("'"):
-					return self.rotateC(board, square)
-				else:
-					return self.rotateCC(board, square)
-	def placePiece(self, board, color):
-		pass3 = False
-		while pass3 == 0:
-			pass0 = False
-			pass1 = False
-			pass2 = False
-			rawloc=raw_input("Location (e.g. A4 or C9): ")
-			if len(rawloc) == 2:
-				pass0 = True
-			if pass0:
-				for number in range(1,10):
-					if rawloc.endswith(str(number)):
-						pass1 = True
-				for letter in ["A", "B", "C", "D"]:
-					if rawloc.startswith(letter):
-						pass2 = True
-			if pass1 and pass2:
-				if rawloc.startswith("A"):
-					square = 0
-				elif rawloc.startswith("B"):
-					square = 1
-				elif rawloc.startswith("C"):
-					square = 2
-				else:
-					square = 3
-				place=int(rawloc[-1])
-				if board[square][place-1]==" ":
-					pass3 = True
-	
-		board[square][place-1] = color
+		for i in range(0,3,2):
+			for j in range(3):
+				board.append(corner[i][j] + corner[i+1][j])
 		return board
 
+	def get1dArray(self):
+		array = self.get2dArray()
+		board = []
+		
+		for row in array:
+			board += row
+		return board
 
-	def winBoards(self, board):
-		hboard = [board[0][:3]  + board[1][:3] , \
-				  board[0][3:6] + board[1][3:6], \
-				  board[0][6:]  + board[1][6:],  \
-				  board[2][:3]  + board[3][:3] , \
-				  board[2][3:6] + board[3][3:6], \
-				  board[2][6:]  + board[3][6:]]
+	def printBoard(self):
+		#make 2d array of each corner
+		board = self.get1dArray()
 
-		vboard = [board[0][0::3] + board[2][0::3] , \
-				  board[0][1::3] + board[2][1::3], \
-				  board[0][2::3] + board[2][2::3], \
-				  board[1][0::3] + board[3][0::3] , \
-				  board[1][1::3] + board[3][1::3], \
-				  board[1][2::3] + board[3][2::3]]
+		prettyBoard = """\
+		{} {} {}|{} {} {}
+		{} {} {}|{} {} {}
+		{} {} {}|{} {} {}
+		-----+-----
+		{} {} {}|{} {} {}
+		{} {} {}|{} {} {}
+		{} {} {}|{} {} {}
+		""".format(*board)
 
-		dboard = [board[1][:8][2::2] + board[2][:8][2::2], \
-				  board[0][0::4]     + board[3][0::4]]
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print(prettyBoard)
 
-		triboard = [board[0][3::4] + [board[2][2]] + board[3][3::4], \
-		            board[0][1::4] + [board[1][6]] + board[3][1::4], \
-		            board[1][:4][1::2] + [board[0][8]] + board[2][:4][1::2], \
-		            board[1][5::2] + [board[3][0]] + board[2][5::2]]
-		return [hboard, vboard, dboard, triboard]
+	def rotate(self, sector, clockwise):
+		#get corner and convert it to 2d array
+		corner2 = [self.gameBoard[sector][i:i + 3] for i in range(0, len(self.gameBoard[sector]), 3)]
+		#rotate corner
+		if clockwise:
+			rotatedCorner2 = list(zip(*corner2[::-1]))
+		else:
+			rotatedCorner2 = list(zip(*corner2))[::-1]
+		#convert back to 1d array
+		corner = [j for i in rotatedCorner2 for j in i]
+		#add the corner back
+		self.gameBoard[sector] = corner
 
-	def checkGameOver(self, boards):
-		winners =[]
+	def rotateSquare(self):
+		sectors = {"A": 0, "B": 1, "C": 2, "D": 3}
+		clockwise = {"'": True, '"': False}
+		while True:
+			rawRot = input("Rotation (e.g. A' or C\"): ")
+			if len(rawRot) == 2:
+				if (rawRot[0] in sectors) and (rawRot[1] in clockwise):
+					self.rotate(sectors[rawRot[0]], clockwise[rawRot[1]])
+					self.placing = True
+					return
+
+	def placePiece(self):
+		sectors = {"A": 0, "B": 1, "C": 2, "D": 3}
+		numbers = [str(i) for i in range(1, 10)]
+		while True:
+			rawLoc = input("Location (e.g. A4 or C9): ")
+			if len(rawLoc) == 2:
+				if (rawLoc[0] in sectors) and (rawLoc[1] in numbers):
+					if self.gameBoard[sectors[rawLoc[0]]][int(rawLoc[1]) - 1] == " ":
+						self.gameBoard[sectors[rawLoc[0]]][int(rawLoc[1]) - 1] = self.ps[self.player]
+						self.placing = False
+						return
+
+	def diagonalsPos(self, matrix):
+		#Get positive diagonals, going from bottom-left to top-right.
+		result = []
+		for di in ([(j, i - j) for j in range(6)] for i in range(11)):
+			result.append([matrix[i][j] for i, j in di if i >= 0 and j >= 0 and i < 6 and j < 6])
+		return result
+
+	def diagonalsNeg(self, matrix):
+		#Get negative diagonals, going from top-left to bottom-right.
+		result = []
+		for di in ([(j, i + j - 5) for j in range(6)] for i in range(11)):
+			result.append([matrix[i][j] for i, j in di if i >= 0 and j >= 0 and i < 6 and j < 6])
+		return result
+
+	def winBoards(self):
+		board = self.get2dArray()
+
+		rows = board
+		columns = list(zip(*board))
+		diagPos = self.diagonalsPos(board)
+		diagNeg = self.diagonalsNeg(board)
+
+		return (rows + columns + diagNeg + diagPos)
+
+	def checkGameOver(self):
+		boards = self.winBoards()
+		winners = []
 		for board in boards:
-			for solution in range(len(board)):
-				#result_list = []
-				current = board[solution][0]
-				count = 0
-				for value in board[solution]:
-				    if value == current:
-				        count += 1
-				    else:
-				        #result_list.append((current, count))
-				        if count >= 5 and current != " ":
-				        	winners.append(current)
-				        current = value
-				        count = 1
-				#result_list.append([current, count])
-				if count >= 5 and current != " ":
-				    winners.append(current)
+			first = ""
+			count = 0
+			for place, i in enumerate(board):
+				if board[place] == first:
+					count += 1
+				else:
+					first = board[place]
+					count = 1
+				if count >= 5 and board[place] != " ":
+					winners.append(board[place])
+					self.playing = False
+
 		if "B" in winners and "W" in winners:
-			return "BW"
+			print("It's a tie! Good game!")
 		elif "B" in winners:
-			return "B"
+			print("Black wins! Good game!")
 		elif "W" in winners:
-			return "W"
-		return None
+			print("White wins! Good game!")
 
-	def prettyCheckGameover(self, board):
-		CGO=self.checkGameOver(self.winBoards(board))
-		if CGO == "B":
-			print "Black wins! Good game!"
-			return True
-		elif CGO == "W":
-			print "White wins! Good game!"
-			return True
-		elif CGO == "BW":
-			print "It's a tie! Good game!"
-			return True
-		over = True
-		for x in range(4):
-			for y in board[x]:
-				if y == " ":
-					over = False
-		if over:
-			print "It's a tie! Good game!"
-			return True
-
-		return False
+		#check if the board's full
+		full = True
+		for corner in self.gameBoard:
+			for place in corner:
+				if place == " ":
+					full = False
+		if full and self.playing:
+			print("It's a tie! Good game!")
+			self.playing = False
 
 	def play(self):
-		system('cls' if name == 'nt' else 'clear')
-		gameBoard=self.genBoard()
-		playing = True
-		self.printBoard(gameBoard)
-		while playing:
-			#white's turn
-			print "White's Turn\n"
-			gameBoard=self.placePiece(gameBoard, "W")
-			self.printBoard(gameBoard)
-			if self.prettyCheckGameover(gameBoard):
-				playing = False
-			if playing:
-				print "White's Turn\n"
-				gameBoard=self.rotateSquare(gameBoard)
-				self.printBoard(gameBoard)
-				if self.prettyCheckGameover(gameBoard):
-					playing = False
-				if playing:
-					print "Black's Turn\n"
-					gameBoard=self.placePiece(gameBoard, "B")
-					self.printBoard(gameBoard)
-					if self.prettyCheckGameover(gameBoard):
-						playing = False
-					if playing:
-						print "Black's Turn\n"
-						gameBoard=self.rotateSquare(gameBoard)
-						self.printBoard(gameBoard)
-						if self.prettyCheckGameover(gameBoard):
-							playing = False
-							
+		self.printBoard()
+		while self.playing:
+			print("{}'s Turn".format(self.players[self.player]))
+			if self.placing:
+				self.placePiece()
+			else:
+				self.rotateSquare()
+				self.switchPlayers()
+
+			self.printBoard()
+			self.checkGameOver()
+
 if __name__ == "__main__":
-	Pentago()
+	Pentago().play()
